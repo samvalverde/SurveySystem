124d827dea79472483257d30790ab5516ae91c0b
crisac15 <cacuna@estudiantec.cr>
2024-04-19 01:56:51 -0600

    new collection

--- python-flask-restapi/mongo-init-scripts/init-mongo.js
+++ python-flask-restapi/mongo-init-scripts/init-mongo.js
@@ -55,3 +55,42 @@ db.encuestas.insertMany([
     ],
   },
 ]);
+
+// Crear la colección "respuestas" si no existe
+db.createCollection("respuestas");
+
+// Crear el índice único en encuesta_id y usuario_id
+db.respuestas.createIndex({ encuesta_id: 1, usuario_id: 1 }, { unique: false });
+
+// Insertar los documentos de respuesta
+db.respuestas.insertMany([{
+  encuesta_id: "1",
+  usuario_id: "1",
+  preguntas: [
+    {
+    pregunta_num: "1",
+    texto_pregunta: "¿Cómo calificarías nuestro servicio?",
+    respuesta: "Regular",
+    },{
+      pregunta_num: "2",
+      texto_pregunta: "¿Recomendarías nuestro producto?",
+      respuesta: "Sí",
+      },
+    ]
+  },
+    {
+    encuesta_id: "2",
+    usuario_id: "1",
+    preguntas: [
+      {
+      pregunta_num: "1",
+      texto_pregunta: "¿Cuál es tu opinión sobre nuestro nuevo servicio?",
+      respuesta: "Es muy bueno",
+      },{
+        pregunta_num: "2",
+        texto_pregunta: "¿Qué tan satisfecho estás con la atención al cliente?",
+        respuesta: "Muy satisfecho",
+        },
+      ],
+    },
+]);
