178efe559b114cefaf5995e733cd87b1f2bf25dc
samuel valverde <savalverde@estudiantec.cr>
2024-04-19 20:11:55 -0600

    ...

--- python-flask-restapi/tests/test_dbMongo.py
+++ python-flask-restapi/tests/test_dbMongo.py
@@ -2,14 +2,12 @@ import pytest
 from unittest.mock import MagicMock
 from surveyService import SurveyService
 from mongomock import MongoClient
+from dbMongo import MongoDatabase  # Importa la clase MongoDatabase
 
 '''
 client = MongoClient("mongo", 27017, username="root", password="password")
 mongo_db = MongoDatabase(client)
 '''
-
-# Hay que instanciar MongoDatabase para llamar a los metodos reales
-
 
 @pytest.fixture
 def mock_mongo_collection():
@@ -18,6 +16,9 @@ def mock_mongo_collection():
 @pytest.fixture
 def survey_service(mock_mongo_collection):
     return SurveyService(mock_mongo_collection)
+
+# Hay que instanciar MongoDatabase para llamar a los metodos reales
+mongoDb = MongoDatabase(mock_mongo_collection)
 
 def test_insert_encuesta(survey_service, mock_mongo_collection):
     encuesta_data = {
@@ -47,3 +48,12 @@ def test_insert_encuesta(survey_service, mock_mongo_collection):
 
 def test_get_encuestas(survey_service, mock_mongo_collection):
     assert survey_service.get_encuestas() == []
+
+'''
+def test_insert_pregunta_None():
+    try:
+        insert_pregunta(None)
+    except Exception as e:
+        assert str(e) == "Error: El JSON proporcionado no es válido"
+    assert False
+'''
