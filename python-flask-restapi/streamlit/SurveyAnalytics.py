323f9cd65ab05a54026762c0ca6b8e215161466f
samuel valverde <savalverde@estudiantec.cr>
2024-06-06 16:07:07 -0600

    conexion neo

--- python-flask-restapi/streamlit/SurveyAnalytics.py
+++ python-flask-restapi/streamlit/SurveyAnalytics.py
@@ -1,13 +1,11 @@
 import os
 import streamlit as st
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, ArrayType, MapType
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, MapType
 from pyspark.sql.functions import explode, col
 from pymongo import MongoClient
-import pandas as pd
 from pyspark.sql import SparkSession
 from neo4j import GraphDatabase
 
-# La función itera sobre los datos de encuestas y respuestas, y utiliza transacciones para crear los nodos y relaciones.
 def create_nodes_and_relationships(driver, data_encuestas, data_respuestas):
     def create_encuesta(tx, id_encuesta, titulo_encuesta):
         query = """
@@ -64,42 +62,15 @@ def create_nodes_and_relationships(driver, data_encuestas, data_respuestas):
                 respuesta_value = respuesta["respuesta"]
                 session.write_transaction(create_respuesta, usuario_id, texto_pregunta, respuesta_value)
 
-
-
 def createDF(spark, collection):
-    # Obtener todos los documentos de la colección
     documents = list(collection.find())
-
-    # Si no hay documentos, retornar un DataFrame vacío
     if not documents:
         print("No hay documentos en la colección.")
         return spark.createDataFrame([], schema=StructType([]))
-    
-    # Eliminar el campo _id de cada documento
+
     for doc in documents:
         doc.pop('_id', None)
 
-    # Convertir listas y diccionarios a cadenas de texto
-    #for doc in documents:
-    #    for key, value in doc.items():
-    #        if isinstance(value, (list, dict)):
-    #            doc[key] = str(value)
-
-
-    # Inferir el esquema basado en el primer documento
-    #sample_doc = documents[0]
-    #schema = StructType([StructField(key, StringType(), True) for key in sample_doc.keys()])
-
-    # Crear una lista de diccionarios con los documentos
-    #data = [doc for doc in documents]
-
-    # Crear un DataFrame de pandas
-    #pandas_df = pd.DataFrame(data)
-
-    # Crear un DataFrame de Spark a partir del DataFrame de pandas
-    #spark_df = spark.createDataFrame(pandas_df, schema=schema)
-
-    # Crear el esquema de Spark basado en los documentos
     schema = StructType()
     for key in documents[0].keys():
         if isinstance(documents[0][key], list):
@@ -107,20 +78,16 @@ def createDF(spark, collection):
         else:
             schema.add(StructField(key, StringType(), True))
 
-    # Crear un DataFrame de Spark directamente desde los documentos
     spark_df = spark.createDataFrame(documents, schema=schema)
-    
     return spark_df
 
 def showDF(encuestasDF, respuestasDF):
     try:
-        # Flatten the preguntas column for encuestasDF
         encuestasDF = encuestasDF.withColumn("preguntas", explode("preguntas"))
         encuestasDF = encuestasDF.select("*", col("preguntas")["texto_pregunta"].alias("texto_pregunta"),
                                                col("preguntas")["posibles_respuestas"].alias("posibles_respuestas"),
                                                col("preguntas")["tipo_pregunta"].alias("tipo_pregunta"))
         encuestasDF = encuestasDF.drop("preguntas")
-        # Flatten the respuestas column for respuestasDF
         respuestasDF = respuestasDF.withColumn("respuestas", explode("respuestas"))
         respuestasDF = respuestasDF.select("*", col("respuestas")["respuesta"].alias("respuesta"),
                                                col("respuestas")["texto_pregunta"].alias("texto_pregunta"),
@@ -133,7 +100,7 @@ def showDF(encuestasDF, respuestasDF):
         st.write("Data from MongoDB (Encuestas):")
         st.write(f"Number of rows in encuestasDF: {encuestasDF.count()}")
         if encuestasDF.count() < 1000:
-            st.dataframe(encuestasDF.limit(100).toPandas())  # Limitar el número de filas a convertir a Pandas
+            st.dataframe(encuestasDF.limit(100).toPandas())
         else:
             st.write("DataFrame is too large to display with Pandas.")
     except Exception as e:
@@ -143,17 +110,15 @@ def showDF(encuestasDF, respuestasDF):
         st.write("Data from MongoDB (Respuestas):")
         st.write(f"Number of rows in respuestasDF: {respuestasDF.count()}")
         if respuestasDF.count() < 1000:
-            st.dataframe(respuestasDF.limit(100).toPandas())  # Limitar el número de filas a convertir a Pandas
+            st.dataframe(respuestasDF.limit(100).toPandas())
         else:
             st.write("DataFrame is too large to display with Pandas.")
     except Exception as e:
         st.write(f"Error while converting respuestasDF to Pandas: {e}")
 
-
 def main():
     st.title("Survey Analytics")
 
-    # Initialize Spark
     spark = SparkSession.builder \
             .appName("SurveyAnalytics") \
             .master("spark://spark:7077") \
@@ -171,26 +136,32 @@ def main():
     encuestasCollection = mongoDB["encuestas"]
     respuestasCollection = mongoDB["respuestas"]
 
-    # Creates both dataframes for encuestasCollection and respuestasCollection
     encuestasDF = createDF(spark, encuestasCollection)
     respuestasDF = createDF(spark, respuestasCollection)
-
-    #Shows both dataframes for encuestasCollection and respuestasCollection using streamlit
     showDF(encuestasDF, respuestasDF)
 
+    data_encuestas = encuestasDF.collect()
+    data_respuestas = respuestasDF.collect()
 
-    # Initialize Neo4j
     neo4j_uri = "bolt://neo4j:7687"
-    neo4j_user = os.getenv("NEO4J_USER")
-    neo4j_password = os.getenv("NEO4J_PASSWORD")
+    neo4j_user = "neo4j"
+    #os.getenv("NEO4J_USER")
+    neo4j_password = "password"
+    #os.getenv("NEO4J_PASSWORD")
+
+    st.write(f"Neo4j User: {neo4j_user}")
+    st.write(f"Neo4j Password: {neo4j_password}")
+
+    # Hardcode credentials for testing
+    # neo4j_user = "your_username"
+    # neo4j_password = "your_password"
+
     driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
 
     st.write("Neo4j and Spark initialized.")
-
-    # Create nodes and relationships in Neo4j
-    #create_nodes_and_relationships(driver, encuestasDF, respuestasDF)
-
+    create_nodes_and_relationships(driver, data_encuestas, data_respuestas)
     st.write("Nodes and relationships created in Neo4j.")
 
 if __name__ == "__main__":
     main()
+
